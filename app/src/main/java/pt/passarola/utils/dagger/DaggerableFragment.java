25c0b562ffc89fdcf4a6373b36424af30f9f2482
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2015-11-03 23:57:10 +0000

    Use dagger to inject fragments

---
+++ app/src/main/java/pt/passarola/utils/dagger/DaggerableFragment.java
@@ -0,0 +1,26 @@
+package pt.passarola.utils.dagger;
+
+/**
+ * Created by ruigoncalo on 31/10/15.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import pt.passarola.App;
+
+public class DaggerableFragment extends Fragment implements Daggerable {
+
+    @Override public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        inject(this);
+    }
+
+    @Override
+    public void inject(Object object) {
+        App.obtain(getActivity()).inject(object);
+    }
+
+    @Override public void inject(Object object, Object... modules) {
+        App.obtain(getActivity()).inject(object, modules);
+    }
+}
