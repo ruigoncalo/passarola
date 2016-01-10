582a70ce26d4d770293dfe3d5fb0ca419898cbb5
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-01-10 22:25:34 +0000

    Create about layout

--- app/src/main/java/pt/passarola/ui/AboutActivity.java
+++ app/src/main/java/pt/passarola/ui/AboutActivity.java
@@ -1,11 +1,14 @@
 package pt.passarola.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pt.passarola.R;
 import pt.passarola.services.dagger.DaggerableAppCompatActivity;
 
@@ -13,6 +16,8 @@ import pt.passarola.services.dagger.DaggerableAppCompatActivity;
  * Created by ruigoncalo on 22/10/15.
  */
 public class AboutActivity extends DaggerableAppCompatActivity {
+
+    private static final String FB_PASSAROLA_ID = "713214482083435";
 
     @Bind(R.id.toolbar) Toolbar toolbar;
 
@@ -34,13 +39,38 @@ public class AboutActivity extends DaggerableAppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setupToolbar(){
+    private void setupToolbar() {
         setSupportActionBar(toolbar);
-        if(getSupportActionBar() != null) {
+        if(getSupportActionBar() != null){
             getSupportActionBar().setTitle(R.string.about);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeButtonEnabled(true);
         }
     }
 
+    @OnClick(R.id.button_email)
+    public void onEmailButtonClick(){
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        emailIntent.setType("vnd.android.cursor.item/email");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] {"info@passarola.pt"});
+        startActivity(Intent.createChooser(emailIntent, getString(R.string.send_email_using)));
+    }
+
+    /**
+     * Load facebook page if facebook app is installed
+     * open browser otherwise
+     */
+    @OnClick(R.id.button_facebook)
+    public void onFacebookButtonClick(){
+        Intent intent;
+        try {
+            intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://page/" + FB_PASSAROLA_ID));
+            startActivity(intent);
+        } catch (Exception e) {
+            intent =  new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/" + FB_PASSAROLA_ID));
+            startActivity(intent);
+        }
+    }
 }
