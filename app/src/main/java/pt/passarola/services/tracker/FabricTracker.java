313c5f86563ad8dce2e3a62746f85c1959979037
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2016-01-14 23:30:57 +0000

    Add Fabric tracker

--- app/src/main/java/pt/passarola/services/tracker/FabricTracker.java
+++ app/src/main/java/pt/passarola/services/tracker/FabricTracker.java
@@ -1,7 +1,27 @@
 package pt.passarola.services.tracker;
 
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.CustomEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by ruigoncalo on 14/01/16.
  */
 public class FabricTracker {
+
+    public void trackEvent(String eventName){
+        Answers.getInstance().logCustom(new CustomEvent(eventName));
+    }
+
+    public void trackEvent(String eventName, HashMap<String, String> attributes){
+        CustomEvent customEvent = new CustomEvent(eventName);
+
+        for(Map.Entry<String, String> entry : attributes.entrySet()){
+            customEvent.putCustomAttribute(entry.getKey(), entry.getValue());
+        }
+
+        Answers.getInstance().logCustom(customEvent);
+    }
 }
