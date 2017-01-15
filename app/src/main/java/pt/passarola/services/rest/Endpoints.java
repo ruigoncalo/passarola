30c0261023666feb69a5a5f4d2bb6ced073cc956
ruigoncalo <ruimiguelgoncalo@gmail.com>
2017-01-15 22:06:22 +0000

    Update passarola api

--- app/src/main/java/pt/passarola/services/rest/Endpoints.java
+++ app/src/main/java/pt/passarola/services/rest/Endpoints.java
@@ -4,7 +4,7 @@ package pt.passarola.services.rest;
  * Created by ruigoncalo on 22/10/15.
  */
 public class Endpoints {
-    public final static String baseUrl = "http://www.passarola.pt/api";
+    public final static String baseUrl = "http://www.api.passarola.pt";
     public final static String places = "/places";
     public final static String beers = "/beers";
 }
